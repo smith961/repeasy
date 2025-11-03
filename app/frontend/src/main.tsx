8c2d45beafba3ff14393ad1b923d6b2fab815ada
Smith <smithbabawande@gmail.com>
2025-11-03 21:09:40 +0100

    latest branch

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@mysten/dapp-kit/dist/index.css";
-import "@radix-ui/themes/styles.css";
-
-import {  WalletProvider } from "@mysten/dapp-kit";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Theme } from "@radix-ui/themes";
 import App from "./App.tsx";
 
 
-const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme appearance="dark">
-      <QueryClientProvider client={queryClient}>
+    
+      
         
-          <WalletProvider autoConnect>
+          
             <App />
-          </WalletProvider>
+          
         
-      </QueryClientProvider>
-    </Theme>
+      
+    
   </React.StrictMode>,
 );
